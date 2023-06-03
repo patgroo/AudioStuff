5e900cfa33c2dd7bfc0d52edefbe81dba7c21391
patgroo <63114232+patgroo@users.noreply.github.com>
2023-06-03 20:15:22 +0200

    Add files via upload

---
+++ PyAudio_get_Devicelist.py
@@ -0,0 +1,16 @@
+import pyaudio
+
+def print_available_devices():
+    p = pyaudio.PyAudio()
+    info = p.get_host_api_info_by_index(0)
+    num_devices = info.get('deviceCount')
+
+    print("Available audio devices:")
+    for i in range(num_devices):
+        device_info = p.get_device_info_by_host_api_device_index(0, i)
+        device_name = device_info.get('name')
+        print(f"Device {i}: {device_name}")
+
+    p.terminate()
+
+print_available_devices()
